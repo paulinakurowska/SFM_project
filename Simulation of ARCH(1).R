5a74f6ba363986e453c7888df992b40d36b8e6ea
monsterBB <wang.yaoyaom@163.com>
2017-12-18 16:21:30 +0100

    Update Simulation of ARCH(1).R

--- Simulation of ARCH(1).R
+++ Simulation of ARCH(1).R
@@ -7,29 +7,6 @@ if(!require("fGarch")){install.packages("fGarch")};  library(fGarch)
 if(!require("grid")){install.packages("grid")};  library(grid)
 if(!require("gridExtra")){install.packages("gridExtra")};  library(gridExtra)
 
-#set seed
-set.seed(123)
-#simulated ARCH(1) model with n=100 with alpha=0.9 
-model100 <- garchFit(~garch(1), data = garchSim(spec = garchSpec(model = list(alpha = 0.9, beta = 0)), n=100),cond.dist = "QMLE")
-model100
-summary(model100)
-#plot of Time Series
-plot(model100,which = 1)
-#plot of Standardized Residuals
-plot(model100,which = 9)
-#Quantile-Quantile Plot of Standardized Residuals
-plot(model100,which = 13)
-
-#now the same for n=1000
-model1000 <- garchFit(~garch(1), data = garchSim(spec = garchSpec(model = list(alpha = 0.9, beta = 0)), n=1000),cond.dist = "QMLE")
-model1000
-summary(model1000)
-#plot of Time Series
-plot(model1000,which = 1)
-#plot of Standardized Residuals
-plot(model1000,which = 9)
-#Quantile-Quantile Plot of Standardized Residuals
-plot(model1000,which = 13)
 #Simulation: function takes an input for # replications =k (by defeult it is 1000)
 # and seed (by default it is 123)
 simulation<-function(size_of_dataset, k=1000,s=123){
